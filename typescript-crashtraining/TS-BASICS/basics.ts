604eaed41a52aaf31b889c76f961480f36845b23
omarbadrandev <omarbadranm93@hotmail.com>
2022-06-14 16:49:00 +0200

    small function training

--- typescript-crashtraining/TS-BASICS/basics.ts
+++ typescript-crashtraining/TS-BASICS/basics.ts
@@ -26,4 +26,22 @@ const myPerson: Person = {
   cool: true
 };
 
+const ids: Record<number, string> = {
+  10: "a",
+  20: "b"
+};
+
+ids[30] = "c";
+
+if (ids[30] === "D") {
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+[1, 2, 3].forEach((v) => console.log(v));
+
+const out: number[] = [4, 5, 6].map((v) => v * 10);
+console.log(out);
 console.log(myPerson.cool);
