5b376470e2722b37aa9bb03d97fac2b5a4e1c70e
omarbadrandev <omarbadranm93@hotmail.com>
2022-06-14 14:10:05 +0200

    mapById using Record

--- typescript-crashtraining/utilityTypes/utility-types-1.ts
+++ typescript-crashtraining/utilityTypes/utility-types-1.ts
@@ -2,6 +2,7 @@ interface MyUser {
   name: string;
   id: string;
   email?: string;
+  phone?: string;
 }
 
 //Partial Makes all user interface values optional
@@ -23,4 +24,24 @@ console.log(
     },
     { email: "omarBadranm293@hotmail.com" }
   )
+);
+type RequiredMyUser = Required<MyUser>;
+type JustEmailAndName = Pick<MyUser, "email" | "name">;
+
+// Constructs an object type whose property keys are Keys and whose property values are Type.
+// This utility can be used to map the properties of a type to another type.
+const mapById = (users: MyUser[]): Record<string, MyUser> => {
+  return users.reduce((accomulator, v) => {
+    return {
+      ...accomulator,
+      [v.id]: v
+    };
+  }, {});
+};
+
+console.log(
+  mapById([
+    { id: "foo", name: "Mr.Foo" },
+    { id: "baz", name: "BAZ" }
+  ])
 );
